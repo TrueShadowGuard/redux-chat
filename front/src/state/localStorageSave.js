e6ce47ca8d0a044918f22b44b6faf6869b951d82
Vadim <playvarian@gmail.com>
2021-09-28 18:48:20 +0300

    fixed userId was not save in localStorage

--- front/src/state/localStorageSave.js
+++ front/src/state/localStorageSave.js
@@ -1,15 +1,18 @@
 import {subscribe} from './store';
+import store from './store';
 
 const paths = ['isDarkTheme', 'username', 'userId'];
+
+const savedStorePart = JSON.parse(localStorage.getItem('redux')) || {};
+savedStorePart.userId = store.getState().userId;
+localStorage.setItem('redux', JSON.stringify(savedStorePart))
 
 paths.forEach(path => {
   subscribe(path, state => {
     const savedStorePart = JSON.parse(localStorage.getItem('redux')) || {};
-    savedStorePart[path] = state[path]
+    savedStorePart[path] = state[path];
     localStorage.setItem('redux', JSON.stringify(savedStorePart));
   })
 });
-
-console.log('localStorageSave');
 
 
