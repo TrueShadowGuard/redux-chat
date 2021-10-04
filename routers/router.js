7d37560d067577e2baa97e39959ffa96791b9de0
Vadim <playvarian@gmail.com>
2021-10-04 22:25:52 +0300

    fixed router

--- routers/router.js
+++ routers/router.js
@@ -8,10 +8,10 @@ const router = express();
 router.use('/auth', authRouter);
 router.use('/users', usersRouter)
 
-router.use(express.static(path.join(__dirname, 'front', 'build')));
+router.use(express.static(path.join(__dirname, '..', 'front', 'build')));
 
 router.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'front', 'build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '..', 'front', 'build', 'index.html'));
 });
 
 module.exports = router;
